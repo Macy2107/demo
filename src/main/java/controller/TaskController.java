97d5c634121c251a6e308b5197829ba82dcd3c83
Hai My Bui Huynh <macy.buihuynh@email.com>
2026-09-05 16:38:28 +0700

    add id

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -28,8 +28,8 @@ public class TaskController {
     }
 
     // 3. Get Task by ID
-    @GetMapping("/tasks")
-    public Task getTaskById(@PathVariable Long id) {
+    @GetMapping("/tasks{id}")
+    public model.Task getTaskById(@PathVariable Long id) {
         return taskService.getTaskById(id);
     }
 
